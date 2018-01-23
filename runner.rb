3857f118a48a9d2ef6188c78f9107721e23da27a
je27 <jocmoya@gmail.com>
2018-01-22 20:27:23 -0600

    migration

--- runner.rb
+++ runner.rb
@@ -8,6 +8,7 @@ puts "      [1] See all hot sauces"
 puts "      [2] Select one hot sauce"
 puts "      [3] Create your own custom hot sauce"
 puts "      [4] Update a hot sauce order"
+puts "      [5] Delete an order. Cannot be undone!"
 
 input_option = gets.chomp
 
@@ -35,6 +36,8 @@ elsif input_option == "3"
   client_params[:heat_level] = gets.chomp
   print "Description:  "
   client_params[:description] = gets.chomp
+  print "In Stock: "
+  client_params[:in_stock] = gets.chomp
   print "Link to bottle image:  "
   client_params[:image_url] = gets.chomp
 
@@ -48,5 +51,43 @@ elsif input_option == "3"
   puts JSON.pretty_generate(product)
 
 elsif input_option == "4"
-  
+  print "Enter product ID: "
+  input_id = gets.chomp
+
+  response = Unirest.get("http://localhost:3000/products/#{input_id}")
+  product = response.body
+
+  print "Name: (#{product["name"]})"
+  client_params[:name] = gets.chomp
+  print "Price: (#{product["price"]})"
+  client_params[:price] = gets.chomp
+  print "Ingredients: (#{product["ingredients"]})"
+  client_params[:ingredients] = gets.chomp
+  print "Heat Level: (#{product["heat_level"]})"
+  client_params[:heat_level] = gets.chomp
+  print "Description: (#{product["description"]})"
+  client_params[:description] = gets.chomp
+  print "In Stock: (#{product["in_stock"]})"
+  client_params[:in_stock] = gets.chomp
+  print "Link to bottle image: (#{product["image_url"]})"
+  client_params[:image_url] = gets.chomp
+
+  client_params.delete_if { |key, value| value.empty? }
+
+  response = Unirest.patch(
+                            'http://localhost:3000/products/#{input_id}',
+                            parameters: client_params
+                          )
+
+product = response.body
+puts JSON.pretty_generate(product)
+
+elsif input_option == "5"
+  print "Enter a product ID of product you want to delete:"
+  input_id = gets.chomp
+
+  response = Unirest.delete("http://localhost:3000/products/#{input_id}")
+
+  product = response.body
+  puts product["message"]
 end
